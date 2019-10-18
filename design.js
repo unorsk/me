0e797576bacc8d07a16200dfbb5621167c8c692e
andrii <andrii.uvarov@nav.no>
2019-10-18 11:51:16 +0200

    Updates

--- design.js
+++ design.js
@@ -212,7 +212,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60126" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65305" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
